cf6ea2be19d67158dffe3e90bc4be8951b676320
RC_Student_lab <RC_Student_lab@10.0.0.28>
2024-09-30 19:20:11 +0200

    Poe 1

--- src/main/java/com/mycompany/registrationandlogin/RegistrationAndLogin.java
+++ src/main/java/com/mycompany/registrationandlogin/RegistrationAndLogin.java
@@ -94,8 +94,7 @@ public class RegistrationAndLogin {
  //}
  return true;
  }
- 
-   public static boolean returnLoginstatus(boolean Logged){
+ public static boolean returnLoginstatus(boolean Logged){
  JOptionPane.showMessageDialog(null, "Click "+ "ok to login");
  if (loginUser(userName, Password)){
  JOptionPane.showMessageDialog(null,"Welcome"+
