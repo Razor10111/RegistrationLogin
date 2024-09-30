2b9d66c8dc996abfc03d96b8db933faa8952b349
RC_Student_lab <RC_Student_lab@10.0.0.28>
2024-09-30 22:34:09 +0200

    Poe 1

--- src/main/java/com/mycompany/registrationandlogin/RegistrationAndLogin.java
+++ src/main/java/com/mycompany/registrationandlogin/RegistrationAndLogin.java
@@ -95,6 +95,20 @@ public class RegistrationAndLogin {
  //}
  return true;
  }
+   
+    public static boolean returnLoginstatus(boolean Logged){
+ JOptionPane.showMessageDialog(null, "Click "+ "ok to login");
+ if (loginUser(userName, Password)){
+ JOptionPane.showMessageDialog(null,"Welcome"+
+ ""
+ + " "+
+ name +" "+ surname +" "+"it is good to see you");
+ }
+ else{
+ JOptionPane.showMessageDialog(null, "Incorrect "+ "login details");
+ }
+ return true;
+ }
 }
 
 
