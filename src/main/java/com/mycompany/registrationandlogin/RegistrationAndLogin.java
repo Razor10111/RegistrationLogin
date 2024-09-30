cc6294480f9d24f47c7a3e92781fe6d113207d66
RC_Student_lab <RC_Student_lab@10.0.0.28>
2024-09-30 18:50:30 +0200

    Poe 1

---
+++ src/main/java/com/mycompany/registrationandlogin/RegistrationAndLogin.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrationandlogin;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class RegistrationAndLogin {
+ static String name;
+ static String surname;
+ static String userName ;
+ static String Password;
+ static String Password2;
+ static String user2;
+ 
+    
+ public static void main(String[] args) {
+     
+}
+}
+
+ 
+ 
+
+ 
+      
+    
+  
